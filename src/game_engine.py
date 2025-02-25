1115aecb1ba97ea911c754edebfa9abdcfb2e82d
nikitavoitik <nikitavoitik2006@gmail.com>
2025-02-25 19:59:51 +0100

    Updated server to work with game_engine

--- src/game_engine.py
+++ src/game_engine.py
@@ -9,14 +9,15 @@ def multiply_elements(*args):
             return [x * args[1] for x in args[0]]
         elif isinstance(args[1], list) and isinstance(args[0], (int, float)):
             return [x * args[0] for x in args[1]]
-        
+
         else:
             raise TypeError("Arguments must be either two lists or one list and one number.")
     else:
         raise ValueError("Exactly two arguments are required.")
 
+
 class Player:
-    def __init__(self,color,x_coordinate, y_coordinate, score,radius=15,name="computer",speed=[1,1]):
+    def __init__(self, color, x_coordinate, y_coordinate, score, radius=15, name="computer", speed=[1, 1]):
         self.x = x_coordinate
         self.y = y_coordinate
         self.score = score
@@ -25,8 +26,10 @@ class Player:
         self.y_speed = speed[1]
         self.color = color
         self.radius = radius
+
     def move(self, direction):
-        self.x , self.y = self.x + direction[0]*self.x_speed, self.y + direction[1]*self.y_speed
+        self.x, self.y = self.x + direction[0] * self.x_speed, self.y + direction[1] * self.y_speed
+
     def get_info(self):
         return {
             "player_name": self.name,
@@ -34,39 +37,47 @@ class Player:
             "color": self.color,
             "radius": self.radius,
             "position": [self.x, self.y],
-            "speed": [self.x_speed, self.y_speed] 
+            "speed": [self.x_speed, self.y_speed]
         }
+
+
 class Ball:
-    def __init__(self , x_coordinate, y_coordinate, speed:list , radius,direction=[1,1],color="white"):
+    def __init__(self, x_coordinate, y_coordinate, speed: list, radius, direction=[1, 1], color="white"):
         self.x = x_coordinate
         self.y = y_coordinate
         self.speed = speed
         self.radius = radius
         self.color = color
         self.direction = direction
-        self.initial_speed = [7,7]
+        self.initial_speed = [7, 7]
+
     def update_speed_time(self):
-        self.speed[0] *=.99
-        self.speed[1] *=.99
-    def check_accident_with_wall(self,left_x, right_x, top_y, bottom_y):        
-        if self.x + self.radius > right_x-10 or self.x - self.radius < left_x:
-            self.direction[1]*=-1
-            self.speed[0]*=.7
-            self.speed[1]*=.7
-        if self.y + self.radius > bottom_y-10 or self.y - self.radius < top_y:
-            self.direction[0]*=-1
-            self.speed[0]*=.7
-            self.speed[1]*=.7
-    def check_accident_with_player(self, player:Player):
-        distance=((self.x - player.x)**2 + (self.y - player.y)**2)**0.5
+        self.speed[0] *= .99
+        self.speed[1] *= .99
+
+    def check_accident_with_wall(self, left_x, right_x, top_y, bottom_y):
+        if self.x + self.radius > right_x - 10 or self.x - self.radius < left_x:
+            self.direction[1] *= -1
+            self.speed[0] *= .7
+            self.speed[1] *= .7
+        if self.y + self.radius > bottom_y - 10 or self.y - self.radius < top_y:
+            self.direction[0] *= -1
+            self.speed[0] *= .7
+            self.speed[1] *= .7
+
+    def check_accident_with_player(self, player: Player):
+        distance = ((self.x - player.x) ** 2 + (self.y - player.y) ** 2) ** 0.5
         if abs(distance - player.radius - self.radius) < 7:
-            self.direction[0]*=-1
-            self.direction[1]*=-1
-            self.speed=self.initial_speed
+            self.direction[0] *= -1
+            self.direction[1] *= -1
+            self.speed = self.initial_speed
+
     def check_if_goal(self):
-        pass 
+        pass
+
     def move(self):
-        self.x , self.y = self.x + self.speed[0] * self.direction[0], self.y + self.speed[1] *self.direction[1]
+        self.x, self.y = self.x + self.speed[0] * self.direction[0], self.y + self.speed[1] * self.direction[1]
+
     def get_info(self):
         return {
             "position": [self.x, self.y],
@@ -74,27 +85,33 @@ class Ball:
             "radius": self.radius,
             "color": self.color
         }
+
+
 class Game:
-    def __init__(self, player1:Player , player2:Player):
+    def __init__(self, player1: Player, player2: Player):
         self.screen_width = 400
         self.screen_height = 600
         self.player1 = player1
         self.player2 = player2
-        self.ball=Ball(self.screen_width//2, self.screen_height//2, 5, 10)
+        self.ball = Ball(self.screen_width // 2, self.screen_height // 2, 5, 10)
         self.ball_speed = [1, -1]
+
     def players_update(self, move_info):
-        self.player1.move(move_info["player1"]) 
+        self.player1.move(move_info["player1"])
         self.player2.move(move_info["player2"])
+
     def ball_updates(self):
         self.ball.update_speed_time()
         self.ball.check_accident_with_player(self.player1)
         self.ball.check_accident_with_player(self.player2)
         self.ball.check_accident_with_wall(0, self.screen_width, 0, self.screen_height)
         self.ball.move()
-    def game_step(self,move_info):
+
+    def game_step(self, move_info):
         self.players_update(move_info)
         self.ball_updates()
         return self.get_updates()
+
     def get_updates(self):
         return {
             "player1": self.player1.get_info(),
@@ -103,11 +120,3 @@ class Game:
             "game_screen_width": self.screen_width,
             "game_screen_height": self.screen_height,
         }
-
-
-
-
-
-        
-
-    
---
+++ src/server.py
@@ -0,0 +1,98 @@
+import json
+import threading
+from math import gamma
+
+from websocket_server import WebsocketServer
+import time
+from src.game_engine import Game, Player
+
+player1 = Player(color="red", x_coordinate=200, y_coordinate=200, score=0, radius=15, name="Player1", speed=[1, 1])
+player2 = Player(color="blue", x_coordinate=500, y_coordinate=200, score=0, radius=15, name="Player2", speed=[1, 1])
+
+
+class WebSocketServer:
+    def __init__(self, host='0.0.0.0', port=8080):
+        self.server = WebsocketServer(host=host, port=port)
+        self.clients = []
+        self.game = Game(player1, player2)
+        self.moves_info = {
+            "player1": [0, 0],
+            "player2": [0, 0]
+        }
+
+        self.game_started = False
+
+    def send_to_all(self, message):
+        for c in self.clients:
+            self.server.send_message(c, message)
+
+    def new_client(self, client, server):
+        if len(self.clients) > 2:
+            server.disconnect_client(client)
+            return
+        self.clients.append(client)
+        if len(self.clients) == 2:
+            self.game_started = True
+            self.send_to_all(
+                self.game.get_updates()
+            )
+        print(f"New client connected: {client['id']}")
+
+    def client_left(self, client, server):
+        self.clients.remove(client)
+        print(f"Client disconnected: {client['id']}")
+        self.stop_game()
+
+    def stop_game(self):
+        self.send_to_all("Game Stopped")
+        self.server.server_close()
+
+    def message_received(self, client, server, message):
+        if not self.game_started:
+            return
+        print(f"Message from client {client['id']}: {message} at {time.time()}")
+
+        self.update_moves(client, message)
+        # TODO: pass data to Saman
+        # time.sleep(10)
+        # print(f"Stopped sleeping from client {client['id']}")
+        self.send_to_all(
+            self.game.get_updates()
+        )
+
+    def game_loop(self):
+        # This loop calls game.game_step() every 0.3 seconds
+        while True:
+            if self.game_started:
+                self.game.game_step(self.moves_info)
+                self.send_to_all(self.game.get_updates())
+            time.sleep(0.1)
+
+    def update_moves(self, client, message):
+        moves = json.loads(message)
+        move = list(moves.values())
+        player_index = self.clients.index(client)
+
+        if player_index == 0:
+            self.moves_info['player1'] = move
+        else:
+            self.moves_info['player2'] = move
+
+    def start_server(self):
+        self.server.set_fn_new_client(self.new_client)
+        self.server.set_fn_client_left(self.client_left)
+        self.server.set_fn_message_received(self.message_received)
+
+        loop_thread = threading.Thread(target=self.game_loop, daemon=True)
+        loop_thread.start()
+
+        self.server.run_forever()
+
+
+def main():
+    ws_server = WebSocketServer()
+    ws_server.start_server()
+
+
+if __name__ == "__main__":
+    main()
