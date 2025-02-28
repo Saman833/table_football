79686e5e307e21a0d3fca8b8a70a5d8bfa026dc1
nikitavoitik <nikitavoitik2006@gmail.com>
2025-02-28 13:28:03 +0100

    Separed Client Management from Network

--- src/network.py
+++ src/network.py
@@ -3,10 +3,41 @@ from websocket_server import WebsocketServer
 import time
 
 
+class Client:
+    def __init__(self, client, index):
+        self.client = client
+        self.index = index
+
+class ClientsService:
+    def __init__(self):
+        self.clients = []
+
+    def add_client(self, client):
+        index = 1 if len(self.clients) == 0 else 2
+        self.clients.append(Client(client, index))
+        for client in self.clients:
+            print(client.client)
+
+    def get_count(self):
+        return len(self.clients)
+
+    def remove_client(self, client):
+        self.clients = [c for c in self.clients if c.client != client]
+
+    def get_index(self, client):
+        for c in self.clients:
+            if c.client == client:
+                return c.index
+        return None
+
+    def get_clients(self):
+        return self.clients
+
+
 class Network:
     def __init__(self, host='0.0.0.0', port=8080):
         self.server = WebsocketServer(host=host, port=port)
-        self.clients = []
+        self.clientService: ClientsService = ClientsService()
         self.moves_info = {
             'player1': [0, 0],
             'player2': [0, 0],
@@ -14,22 +45,17 @@ class Network:
 
         self.game_started = False
 
-    def send_to_all(self, message):
-        message = json.dumps(message)
-        for c in self.clients:
-            self.server.send_message(c, message)
-
     def new_client(self, client, server):
-        if len(self.clients) >= 2:
+        if self.clientService.get_count() >= 2:
             server.disconnect_client(client)
             return
-        self.clients.append(client)
-        if len(self.clients) == 2:
+        self.clientService.add_client(client)
+        if self.clientService.get_count() == 2:
             self.game_started = True
         print(f"New client connected: {client['id']}")
 
     def client_left(self, client, server):
-        self.clients.remove(client)
+        self.clientService.remove_client(client)
         print(f"Client disconnected: {client['id']}")
 
     def message_received(self, client, server, message):
@@ -39,12 +65,17 @@ class Network:
 
         moves = json.loads(message)
         move = list(moves.values())
-        player_index = self.clients.index(client)
+        player_index = self.clientService.get_index(client)
 
         if player_index == 0:
             self.moves_info['player1'] = move
         else:
             self.moves_info['player2'] = move
+
+    def send_to_all(self, message):
+        message = json.dumps(message)
+        for c in self.clientService.get_clients():
+            self.server.send_message(c.client, message)
 
     def start_server(self):
         self.server.set_fn_new_client(self.new_client)
